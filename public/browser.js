de01928fbd56f7c3f26e1445a9eb27d5a20167bc
Karna Jit <52819360+Karnajit11@users.noreply.github.com>
2020-06-06 23:40:04 +0600

    Add files via upload

---
+++ public/browser.js
@@ -0,0 +1,58 @@
+function itemTemplete(item){
+    return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+}
+
+//Initial Page Loding
+
+let ourHTML = items.map((item)=>{
+    return itemTemplete(item);
+}).join('');
+
+document.getElementById('item-lists').insertAdjacentHTML('beforeend', ourHTML)
+
+// Create feature
+
+let createField = document.getElementById('create-field');
+document.getElementById('create-form').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    axios.post('/create-item', {text: createField.value}).then((response)=>{
+      document.getElementById('item-lists').insertAdjacentHTML('beforeend', itemTemplete(response.data));
+      createField.value = '';
+      createField.focus();
+    }).catch(()=>{
+        console.log('Please Try Again Later');
+        
+    })
+})
+document.addEventListener('click', (e)=>{
+    //Update Feature
+    if(e.target.classList.contains('edit-me')){
+        let userInput = prompt("Enter your text", e.target.parentElement.parentElement.querySelector('.item-text').innerHTML);
+        if(userInput){
+            axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id')}).then(()=>{
+                e.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput;
+            }).catch(()=>{
+                console.log('Please Try Again Later');
+                
+            })
+        }
+    }
+    
+    //Delete Feature
+    if(e.target.classList.contains('delete-me')){
+        if(confirm('Do you want to delete permanently?')){
+            axios.post('/delete-item', { id: e.target.getAttribute('data-id')}).then(()=>{
+                e.target.parentElement.parentElement.remove();
+            }).catch(()=>{
+                console.log('Please Try Again Later');
+                
+            })
+        }
+    }
+})
